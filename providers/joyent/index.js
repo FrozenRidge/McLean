3b4722c51bdd7a49d4cdaae637ea11265aee3d42
Peter Braden <peterbraden@peterbraden.co.uk>
2013-11-20 15:42:19 -0800

    Work on joyent stuff

--- providers/joyent/index.js
+++ providers/joyent/index.js
@@ -5,6 +5,7 @@ module.exports = require('../../lib/pkgcloud_provider').create(
   , ["keyId", "The ID of your Joyent Key"]
   , ["key", "The path to your Joyent Key"]
   , ["servername", "The name of the instance"]
+  , ["flavor", "The Joyent image type / dataset (ie:"]
   ]
 )
 
